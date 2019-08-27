56915476b4d58cb6656fb021985ab76bcf0cd3f6
Asnod <n.dlugaszek@gmail.com>
2019-08-27 12:13:21 +0200

    Solved problem with babel-polyfill

--- src/index.js
+++ src/index.js
@@ -1,17 +1,1 @@
-//style.scss import
 import '../src/style.scss';
-
-async function getComponent() {
-
-    const element = document.createElement('div');
-    const { default: _ } = await
-    import ( /* webpackChunkName: "lodash" */ 'lodash');
-
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    return element;
-}
-
-getComponent().then(component => {
-    document.body.appendChild(component);
-});
