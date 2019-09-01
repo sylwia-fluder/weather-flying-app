f7a677fce89e13540c885e8c750cf0a17e4ab4c4
Anna <annapapros21@gmail.com>
2019-09-01 15:33:04 +0200

    date from skyscanner with time

--- src/js/flights/flight.js
+++ src/js/flights/flight.js
@@ -2,9 +2,30 @@ import getPlace from './places';
 import flightData from './flightData';
 const moment = require('moment');
 
-const getFlight = (placeID, departureDate) => {
+const createSession = (placeID, departureDate) => {
   return fetch(
-    `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/PL/PLN/en-GB/WRO-sky/${placeID}/${departureDate}?inboundpartialdate=anytime`,
+    'https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0',
+    {
+      method: 'POST',
+      headers: {
+        'x-rapidapi-host':
+          'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com',
+        'x-rapidapi-key': 'f76b9a08c6msh4342c2bb7ef2877p1c960ajsn0922b19cbacf',
+        'content-type': 'application/x-www-form-urlencoded',
+      },
+      body: `country=PL&currency=PLN&locale=en-GB&originplace=WRO-sky&destinationplace=${placeID}&outbounddate=${departureDate}&adults=1`,
+    }
+  )
+    .then(response => {
+      return response;
+    })
+    .catch(err => {
+      return err;
+    });
+};
+const pollResult = sessionKey => {
+  return fetch(
+    `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/${sessionKey}?sortType=duration&sortOrder=asc&stops=0&pageIndex=0&pageSize=10"`,
     {
       method: 'GET',
       headers: {
@@ -18,37 +39,52 @@ const getFlight = (placeID, departureDate) => {
       return response.json();
     })
     .catch(err => {
+      console.log(err);
       return err;
     });
 };
 
 const getFlightData = async city => {
-  const tomorrowDate = moment()
-    .add(1, 'day')
-    .format()
-    .substr(0, 10);
+  try {
+    const tomorrowDate = moment()
+      .add(1, 'day')
+      .format()
+      .substr(0, 10);
 
-  const dataPlace = await getPlace(city);
-  const placeID = dataPlace.Places[0].PlaceId;
-  const placeName = dataPlace.Places[0].PlaceName;
-  const country = dataPlace.Places[0].CountryName;
-
-  const dataFlight = await getFlight(placeID, tomorrowDate);
-  const minPrice = dataFlight.Quotes[0].MinPrice;
-  const carrierID = dataFlight.Quotes[0].OutboundLeg.CarrierIds[0];
-  const carrierName = dataFlight.Carriers.find(
-    carrier => carrier.CarrierId === carrierID
-  ).Name;
-  const departureDate = dataFlight.Quotes[0].OutboundLeg.DepartureDate;
-
-  return new flightData(
-    city,
-    minPrice,
-    placeName,
-    country,
-    carrierName,
-    departureDate
-  );
+    const dataPlace = await getPlace(city);
+    if (dataPlace.Places.length === 0)
+      throw Error('Nie znaleziono podanego miasta.');
+    const placeID = dataPlace.Places[0].PlaceId;
+    const placeName = dataPlace.Places[0].PlaceName;
+    const country = dataPlace.Places[0].CountryName;
+    const sessionResponse = await createSession(placeID, tomorrowDate);
+    const sessionKey = await sessionResponse.headers
+      .get('location')
+      .substring(64);
+    const dataFlight = await pollResult(sessionKey);
+    const legID = dataFlight.Legs.find(leg => leg.Stops.length === 0).Id;
+    const minPrice = dataFlight.Itineraries.find(
+      itin => itin.OutboundLegId === legID
+    ).PricingOptions[0].Price;
+    const departureDate = dataFlight.Legs.find(leg => leg.Id === legID)
+      .Departure;
+    const arriveDate = dataFlight.Legs.find(leg => leg.Id === legID).Arrival;
+    const carrierID = dataFlight.Legs.find(leg => leg.Id === legID).Carriers[0];
+    const carrierName = dataFlight.Carriers.find(
+      carrier => carrier.Id === carrierID
+    ).Name;
+    return new flightData(
+      city,
+      minPrice,
+      placeName,
+      country,
+      carrierName,
+      departureDate,
+      arriveDate
+    );
+  } catch (error) {
+    return error;
+  }
 };
 
 export default getFlightData;
